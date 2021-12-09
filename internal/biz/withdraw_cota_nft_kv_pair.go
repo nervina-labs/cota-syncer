4b616aa180df2809e43d3a1d8ef132833c91e6d9
shaojunda <shaojunda@gmail.com>
2021-12-09 23:24:59 +0800

    feat: add WithdrawCotaNftKvPairUsecase

---
+++ internal/biz/withdraw_cota_nft_kv_pair.go
@@ -0,0 +1,48 @@
+package biz
+
+import (
+	"context"
+	"github.com/nervina-labs/compact-nft-entries-syncer/internal/logger"
+)
+
+type WithdrawCotaNftKvPair struct {
+	BlockNumber         uint64
+	CotaId              string
+	CotaIdCRC           uint32
+	Total               uint32
+	TokenIndex          uint32
+	OutPoint            string
+	OutPointCrc         uint32
+	State               uint8
+	Configure           uint8
+	Characteristic      string
+	ReceiverLockHash    string
+	ReceiverLockHashCrc uint32
+	LockHash            string
+	LockHashCrc         uint32
+}
+
+type WithdrawCotaNftKvPairRepo interface {
+	CreateWithdrawCotaNftKvPair(ctx context.Context, w *WithdrawCotaNftKvPair) error
+	DeleteWithdrawCotaNftKvPairs(ctx context.Context, blockNumber uint64) error
+}
+
+type WithdrawCotaNftKvPairUsecase struct {
+	repo   WithdrawCotaNftKvPairRepo
+	logger *logger.Logger
+}
+
+func NewWithdrawCotaNftKvPairUsecase(repo WithdrawCotaNftKvPairRepo, logger *logger.Logger) *WithdrawCotaNftKvPairUsecase {
+	return &WithdrawCotaNftKvPairUsecase{
+		repo:   repo,
+		logger: logger,
+	}
+}
+
+func (uc *WithdrawCotaNftKvPairUsecase) Create(ctx context.Context, w *WithdrawCotaNftKvPair) error {
+	return uc.repo.CreateWithdrawCotaNftKvPair(ctx, w)
+}
+
+func (uc *WithdrawCotaNftKvPairUsecase) DeleteByBlockNumber(ctx context.Context, blockNumber uint64) error {
+	return uc.repo.DeleteWithdrawCotaNftKvPairs(ctx, blockNumber)
+}
