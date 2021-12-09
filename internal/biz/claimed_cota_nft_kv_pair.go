ab34bfee2f680a19bba87a915ad673ebb287dfa4
shaojunda <shaojunda@gmail.com>
2021-12-09 23:42:02 +0800

    feat: add ClaimedCotaNftKvPairUsecase

---
+++ internal/biz/claimed_cota_nft_kv_pair.go
@@ -0,0 +1,43 @@
+package biz
+
+import (
+	"context"
+	"github.com/nervina-labs/compact-nft-entries-syncer/internal/logger"
+)
+
+type ClaimedCotaNftKvPair struct {
+	BlockNumber uint64
+	CotaId      string
+	CotaIdCRC   uint32
+	Total       uint32
+	TokenIndex  uint32
+	OutPoint    string
+	OutPointCrc uint32
+	LockHash    string
+	LockHashCrc uint32
+}
+
+type ClaimedCotaNftKvPairRepo interface {
+	CreateClaimedCotaNftKvPair(ctx context.Context, w *ClaimedCotaNftKvPair) error
+	DeleteClaimedCotaNftKvPairs(ctx context.Context, blockNumber uint64) error
+}
+
+type ClaimedCotaNftKvPairUsecase struct {
+	repo   ClaimedCotaNftKvPairRepo
+	logger *logger.Logger
+}
+
+func NewClaimedCotaNftKvPairUsecase(repo ClaimedCotaNftKvPairRepo, logger *logger.Logger) *ClaimedCotaNftKvPairUsecase {
+	return &ClaimedCotaNftKvPairUsecase{
+		repo:   repo,
+		logger: logger,
+	}
+}
+
+func (uc *ClaimedCotaNftKvPairUsecase) Create(ctx context.Context, c *ClaimedCotaNftKvPair) error {
+	return uc.repo.CreateClaimedCotaNftKvPair(ctx, c)
+}
+
+func (uc *ClaimedCotaNftKvPairUsecase) DeleteByBlockNumber(ctx context.Context, blockNumber uint64) error {
+	return uc.repo.DeleteClaimedCotaNftKvPairs(ctx, blockNumber)
+}
