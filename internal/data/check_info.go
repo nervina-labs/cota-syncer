29daddf286e6b086188f2f216de92842858ea76e
shaojunda <shaojunda@gmail.com>
2021-12-09 22:34:07 +0800

    feat: implement CheckInfoRepo

---
+++ internal/data/check_info.go
@@ -0,0 +1,44 @@
+package data
+
+import (
+	"context"
+	"github.com/nervina-labs/compact-nft-entries-syncer/internal/biz"
+	"github.com/nervina-labs/compact-nft-entries-syncer/internal/logger"
+	"gorm.io/gorm"
+)
+
+var _ biz.CheckInfoRepo = (*checkInfoRepo)(nil)
+
+type CheckInfo struct {
+	gorm.Model
+
+	BlockNumber uint64
+	BlockHash   string
+	biz.CheckType
+}
+
+type checkInfoRepo struct {
+	data   *Data
+	logger *logger.Logger
+}
+
+func NewCheckInfoRepo(data *Data, logger *logger.Logger) biz.CheckInfoRepo {
+	return &checkInfoRepo{
+		data:   data,
+		logger: logger,
+	}
+}
+
+func (rp checkInfoRepo) FindOrCreateCheckInfo(ctx context.Context, info *biz.CheckInfo) error {
+	if err := rp.data.db.WithContext(ctx).FirstOrCreate(info, CheckInfo{BlockNumber: info.BlockNumber, CheckType: biz.CheckType(0)}).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+func (rp checkInfoRepo) UpdateCheckInfo(ctx context.Context, info biz.CheckInfo) error {
+	if err := rp.data.db.WithContext(ctx).Save(info).Error; err != nil {
+		return err
+	}
+	return nil
+}
