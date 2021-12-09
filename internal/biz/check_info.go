a7252bd705edc4194822038ae3a0e9df22ef5193
shaojunda <shaojunda@gmail.com>
2021-12-09 22:33:26 +0800

    feat: add CheckInfoUsecase

---
+++ internal/biz/check_info.go
@@ -0,0 +1,47 @@
+package biz
+
+import (
+	"context"
+	"github.com/nervina-labs/compact-nft-entries-syncer/internal/logger"
+)
+
+type CheckType uint8
+
+const (
+	SyncEvent CheckType = iota // SyncEvent = 0
+)
+
+func (t CheckType) String() string {
+	return []string{"sync_event"}[t]
+}
+
+type CheckInfo struct {
+	BlockNumber uint64
+	BlockHash   string
+	CheckType
+}
+
+type CheckInfoRepo interface {
+	FindOrCreateCheckInfo(ctx context.Context, info *CheckInfo) error
+	UpdateCheckInfo(ctx context.Context, info CheckInfo) error
+}
+
+type CheckInfoUsecase struct {
+	repo   CheckInfoRepo
+	logger *logger.Logger
+}
+
+func NewCheckInfoUsecase(repo CheckInfoRepo, logger *logger.Logger) *CheckInfoUsecase {
+	return &CheckInfoUsecase{
+		repo:   repo,
+		logger: logger,
+	}
+}
+
+func (uc *CheckInfoUsecase) FindOrCreate(ctx context.Context, checkInfo *CheckInfo) error {
+	return uc.repo.FindOrCreateCheckInfo(ctx, checkInfo)
+}
+
+func (uc *CheckInfoUsecase) Update(ctx context.Context, checkInfo *CheckInfo) error {
+	return uc.Update(ctx, checkInfo)
+}
