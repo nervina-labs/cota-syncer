2b95360a275a4f6b04b51493a6af177727769650
duanyytop <duanyytop@gmail.com>
2022-02-24 15:13:43 +0800

    feat: Replace cota_id with lock_hash

--- internal/db/migrations/000002_create_issuer_infos.up.sql
+++ internal/db/migrations/000002_create_issuer_infos.up.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS issuer_infos (
     id bigint NOT NULL AUTO_INCREMENT,
     block_number bigint unsigned NOT NULL,
-    cota_id char(40) NOT NULL,
+    lock_hash char(64) NOT NULL,
+    lock_hash_crc int unsigned NOT NULL,
     version varchar(40) NOT NULL,
     `name` varchar(255) NOT NULL,
     avatar varchar(500) NOT NULL,
