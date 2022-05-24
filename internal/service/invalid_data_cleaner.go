5a39c6a49ff900fd37d77d565671b99c8fc5365f
shaojunda <shaojunda@gmail.com>
2022-05-24 13:09:50 +0800

    feat: add invalid data service

---
+++ internal/service/invalid_data_cleaner.go
@@ -0,0 +1,47 @@
+package service
+
+import (
+	"context"
+	"github.com/nervina-labs/cota-nft-entries-syncer/internal/biz"
+	"github.com/nervina-labs/cota-nft-entries-syncer/internal/data"
+	"github.com/nervina-labs/cota-nft-entries-syncer/internal/logger"
+)
+
+var _ Service = (*InvalidDataCleaner)(nil)
+
+type InvalidDataCleaner struct {
+	invalidDataUsecase *biz.InvalidDataUsecase
+	logger             *logger.Logger
+	client             *data.CkbNodeClient
+}
+
+func NewInvalidDataService(invalidDataUsecase *biz.InvalidDataUsecase, logger *logger.Logger, client *data.CkbNodeClient) *InvalidDataCleaner {
+	return &InvalidDataCleaner{
+		invalidDataUsecase: invalidDataUsecase,
+		logger:             logger,
+		client:             client,
+	}
+}
+
+func (i InvalidDataCleaner) Start(ctx context.Context, _ string) error {
+	var blockNumber uint64
+	info, err := i.client.Rpc.GetBlockchainInfo(ctx)
+	if err != nil {
+		return err
+	}
+
+	// TODO: replace block number
+	if info.Chain == "ckb" {
+		blockNumber = 111
+	} else {
+		blockNumber = 4809130
+	}
+
+	return i.invalidDataUsecase.Clean(ctx, blockNumber)
+}
+
+func (i InvalidDataCleaner) Stop(ctx context.Context) error {
+	i.logger.Info(ctx, "invalid data cleaner stopped")
+
+	return nil
+}
