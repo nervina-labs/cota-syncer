b353058630d72086a36bdb75082af62a1ca5a09c
duanyytop <duanyytop@gmail.com>
2022-11-30 16:09:01 +0800

    fix: Update mysql drop syntax

--- internal/db/migrations/000018_remove_joyid_info_nickname.up.sql
+++ internal/db/migrations/000018_remove_joyid_info_nickname.up.sql
@@ -1,3 +1,5 @@
 ALTER TABLE joy_id_infos DROP COLUMN nickname;
 
-ALTER TABLE joy_id_info_versions DROP COLUMN nickname, old_nickname;
+ALTER TABLE joy_id_info_versions 
+    DROP COLUMN old_nickname,
+    DROP COLUMN nickname;
