375cc24ddaa54ea4d4b4ed8eaf0bd1024a79ff13
duanyytop <duanyytop@gmail.com>
2022-12-06 20:35:10 +0800

    feat: Remove uc_sub_key_infos_on_pub_key constraint

--- internal/db/migrations/000019_update_subkey_info.down.sql
+++ internal/db/migrations/000019_update_subkey_info.down.sql
@@ -1,4 +1,3 @@
 ALTER TABLE sub_key_infos ADD CONSTRAINT uc_sub_key_infos_on_pub_key UNIQUE (pub_key);
-ALTER TABLE sub_key_infos DROP CONSTRAINT uc_sub_key_infos_on_lock_hash_pub_key;
 
 ALTER TABLE sub_key_infos DROP COLUMN front_end;
