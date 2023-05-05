7eb417556af07465192d2cd3c30334e555524ae2
Jian <ginkgo985@gmail.com>
2023-05-05 19:40:41 +0200

    feat: add uniq key on cota_id_idx

--- internal/db/migrations/000022_create_token_class_audios.up.sql
+++ internal/db/migrations/000022_create_token_class_audios.up.sql
@@ -3,9 +3,9 @@ CREATE TABLE IF NOT EXISTS token_class_audios (
     cota_id char(40) NOT NULL,
     url varchar(255) NOT NULL DEFAULT '' COMMENT 'audio url',
     name varchar(255) NOT NULL DEFAULT '' COMMENT 'name',
-    idx  tinyint unsigned  NOT NULL DEFAULT 0  COMMENT 'idx',
+    idx  int unsigned  NOT NULL DEFAULT 0  COMMENT 'idx',
     created_at datetime(6) NOT NULL,
     updated_at datetime(6) NOT NULL,
     PRIMARY KEY (id),
-    KEY `idx_cota_id_idx_on_class_audios` (`cota_id`, `idx`),
+    UNIQUE KEY `uc_cota_id_idx_on_class_audios` (`cota_id`,`idx`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
