17ef80a6127965671f7e049660369fd2de4b1bda
shaojunda <shaojunda@gmail.com>
2023-08-02 11:42:54 +0800

    feat: save derivation info from metadata

--- internal/db/migrations/000024_add_derivation_to_sub_key_infos.up.sql
+++ internal/db/migrations/000024_add_derivation_to_sub_key_infos.up.sql
@@ -1,5 +1,7 @@
 ALTER TABLE sub_key_infos ADD COLUMN `derivation_c_id` varchar(1500) NOT NULL DEFAULT '' AFTER `device_type`,
-                         ADD COLUMN `derivation_commitment` varchar(64) NOT NULL DEFAULT '' AFTER `derivation_c_id`;
+                          ADD COLUMN `derivation_commitment` varchar(64) NOT NULL DEFAULT '' AFTER `derivation_c_id`;
 
 ALTER TABLE sub_key_info_versions ADD COLUMN `derivation_c_id` varchar(1500) NOT NULL DEFAULT '' AFTER `device_type`,
-                                 ADD COLUMN `derivation_commitment` varchar(64) NOT NULL DEFAULT '' AFTER `derivation_c_id`;
+                                  ADD COLUMN `old_derivation_c_id` varchar(1500) NOT NULL DEFAULT '' AFTER `derivation_c_id`,
+                                  ADD COLUMN `derivation_commitment` varchar(64) NOT NULL DEFAULT '' AFTER `old_derivation_c_id`,
+                                  ADD COLUMN `old_derivation_commitment` varchar(64) NOT NULL DEFAULT '' AFTER `derivation_commitment`;
